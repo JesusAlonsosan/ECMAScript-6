721b4d9703cc37b30573499652d16d7354bdaee5
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-05-05 21:26:38 -0500

    :construction: add *let*

--- ecma6.js
+++ ecma6.js
@@ -7,3 +7,10 @@ document.write("x = " + x + "<br/>");
 
 var y = 100;
 document.write("y = " + y + "<br/>");
+
+var z = 200;
+document.write("z =" + z + "<br/>");
+
+if (false);
+let w = 200;
+document.write("Hola", ("<br/>"));

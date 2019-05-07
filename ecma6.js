cfacfe068165de0b5249cf0c960da7849c182e22
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-05-07 11:22:34 -0500

    :construction: typeof and string interpolation

--- ecma6.js
+++ ecma6.js
@@ -41,4 +41,21 @@ if (true) {
 document.write("PI" + PI + "<br/>");
 
 
-//
+//typeof operator returns the data type of its operand in the form of a string,number,boolean,symbol, etc.
+
+document.write(typeof true + "<br/>"); //boolean
+document.write(typeof 3.14 + "<br/>"); //number
+document.write(typeof "string" + "<br/>"); //string
+document.write(typeof Symbol() + " <br />"); //symbol
+document.write(typeof { a: 1 } + "<br/>"); //object
+document.write(typeof [1, 2, 3] + "<br/>"); //object
+document.write(typeof undefined + "<br/>"); //undefined
+
+
+//String interpolation " $ {.....} "
+
+
+let fName = "Jesus";
+let eName = "Alonso";
+
+document.write(`${fName} ${eName} <br />`);

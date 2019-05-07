c1e91b5736794967b90e798240f1b427cab50a64
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-05-07 13:41:50 -0500

    :contruction:Template Literals

--- ecma6.js
+++ ecma6.js
@@ -53,6 +53,8 @@ document.write(typeof undefined + "<br/>"); //undefined
 
 
 //String interpolation " $ {.....} "
+//Template literals are enclosed by the back-tick (` `) 
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
 
 let fName = "Jesus";
@@ -60,20 +62,26 @@ let eName = "Alonso";
 
 document.write(`${fName} ${eName} <br />`);
 
+
+
+
 let num1 = 10;
 let num2 = 10;
 
-document.write(`1 * 1 = ${num1 * num2} <br />`);
+document.write(`10 * 100 = ${num1 * num2} <br />`);
+
 
 function doMath(strings, ...values) {
-    if (string[0] === 'Add') {
-        document.write(`${values[0]} + ${values[1]} = 
-        ${values[0]} + ${values[1]} "<br />"`);
-    } else if (string[0] === 'Sub') {
-        document.write(`${values[0]} - ${values[1]} =
-        ${values[0] + $values[1]}<br />`);
+
+    if (strings[0] == 'Add') {
+        document.write(`${values[0]} + ${values[1]} = ${values[0] + 
+            values[1]} <br />`);
+
+    } else if (strings[0] === 'Sub') {
+        document.write(`${values[0]} - ${values[1]} = ${values[0] - 
+            values[1]}<br />`);
     }
 }
 
-
-doMath
+doMath `Add${10} ${20}`;
+doMath `Sub${100} ${20}`;
---
+++ ecma6a.js
@@ -0,0 +1,10 @@
+//spread operator o rest operator ( ... )
+
+
+function myFunc(a, b, ...args) {
+    console.log(a); // 22
+    console.log(b); // 98
+    console.log(args); // [43, 3, 26]
+}
+
+myFunc(22, 98, 43, 3, 26);

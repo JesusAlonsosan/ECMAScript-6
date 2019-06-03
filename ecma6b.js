39170514420997fa5117de9997358a71bf8baa0b
JesusAlonsosan <jesusalonsosan@gmail.com>
2019-06-03 14:25:41 -0500

    lenguage javascript :zap:

---
+++ ecma6b.js
@@ -0,0 +1,29 @@
+// tipos de datos
+
+
+"Bro soul" // string
+
+'Soul bro' // string
+
+
+
+10000000  // number
+10.1
+100.2
+-2.3
+
+
+true    // boolean
+false
+
+
+// array
+
+['joe', 'luis' ,'jose']
+[1, 2, 3, 4]
+[true, false, true, false]
+
+
+//object
+
+
